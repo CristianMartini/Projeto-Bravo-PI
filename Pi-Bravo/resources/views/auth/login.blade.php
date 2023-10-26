64117154439b7d6f503cbc5ba9072a10769042f9
ClaudioShowza <111402444+ClaudioShowza@users.noreply.github.com>
2023-10-25 22:31:04 -0300

    Tela de LogIn/Inscrever-se
    Fizemos a tela de LogIn e Inscrever-se juntando com a tela de home.

---
+++ Pi-Bravo/resources/views/auth/login.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./Css/style.css">
+    <title>Login Bravo</title>
+</head>
+<body>
+
+<x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+
+        <!-- Email Address -->
+        <div>
+            <img src = "{{ asset('imagens/Logotipo_bravo.svg') }}" class = "logopng" />
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Remember Me -->
+        <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            </label>
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            <x-primary-button class="ml-5" class="botaoRegistrar">
+                <a href="{{ route('register') }}" class="no-underline text-white">Registrar-se
+            </a>
+            </x-primary-button>
+            @if (Route::has('password.request'))
+                <a class="botaoRegistrar underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+
+            <x-primary-button class="ml-5 botaoRegistrar">
+                {{ __('Log in') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
+
+</body>
+</html>
--- Pi-Bravo/routes/web.php
+++ Pi-Bravo/routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoriaController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,5 +18,16 @@ use App\Http\Controllers\CategoriaController;
 
 Route::get('/', [ProdutoController::class,'index']);
 
-Route::get('/produto/{produto}', [ProdutoController::class,'show']);
-Route::get('/categorias', 'CategoriaController@index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/produto/{produto}', [ProdutoController::class,'show']);
+    Route::get('/categorias', [CategoriaController::class, 'index']);
+});
+
+require __DIR__.'/auth.php';
