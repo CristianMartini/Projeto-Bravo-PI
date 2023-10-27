af33916a0d106f4da2eabcfa8c80f36a3c167f6b
Cristian martini <cristianmartini.sp@gmail.com>
2023-10-26 21:08:24 -0300

    breeeze

--- Pi-Bravo/routes/web.php
+++ Pi-Bravo/routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoriaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,11 +10,13 @@ use App\Http\Controllers\CategoriaController;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!nop
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/', [ProdutoController::class,'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -26,8 +26,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/produto/{produto}', [ProdutoController::class,'show']);
-    Route::get('/categorias', [CategoriaController::class, 'index']);
 });
 
 require __DIR__.'/auth.php';
