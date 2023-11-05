a01e3bdd72531b129bd114e75c98ce915b7a2c99
ClaudioShowza <111402444+ClaudioShowza@users.noreply.github.com>
2023-11-05 00:22:16 -0300

    teste de carrossel (em andamento)

--- Pi-Bravo/resources/views/home.blade.php
+++ Pi-Bravo/resources/views/home.blade.php
@@ -89,6 +89,32 @@
     </div>
 -->
 
+<div class="container">
+  <div>
+    <div class="content">
+      <h2>Jane Doe</h2>
+      <span>UI & UX Designer</span>
+    </div>
+  </div>
+  <div>
+    <div class="content">
+      <h2>Alex Smith</h2>
+      <span>CEO Expert</span>
+    </div>
+  </div>
+  <div>
+    <div class="content">
+      <h2>Emily New</h2>
+      <span>Web Designer</span>
+    </div>
+  </div>
+  <div>
+    <div class="content">
+      <h2>Lisa Boley</h2>
+      <span>Marketing Coordinator</span>
+    </div>
+  </div>
+</div>
 
 <div class="container container__card container-fluid">
     <div class="owl-carousel">
